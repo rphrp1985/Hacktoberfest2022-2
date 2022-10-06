94bef844cd2fb46e285058731d97c511c8ecc804
rphrp1985 <91199336+rphrp1985@users.noreply.github.com>
2022-10-06 19:41:54 +0530

    Update Bubblesort.cpp

--- Add Code Here/C++/Bubblesort.cpp
+++ Add Code Here/C++/Bubblesort.cpp
@@ -21,8 +21,8 @@ void printArray(int arr[], int size)
 {
 	int i;
 	for (i = 0; i < size; i++)
-		cout << arr[i] << " ";
-	cout << endl;
+		printf("%d ",arr[i]); 
+	printf("\n");
 }
 
 // Driver code
